cca157d42b13600eb9b957ebf5973e4610aec5af
doracozma <doracozma98@gmail.com>
2020-05-16 13:17:41 +0300

    initial commit

---
+++ Helpers/MapHelpers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Gms.Tasks;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using ufinix.Helpers;
+
+namespace Slot.Helpers
+{
+   public class MapHelpers
+    {
+        public async Task<string> FindCoordinateAddress(LatLng position, string mapkey)
+        {
+           
+            string url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.Latitude.ToString() + "," + position.Longitude.ToString() + "&key=" + mapkey;
+            string placeAddress = "";
+            var handler = new HttpClientHandler();
+            HttpClient httpClient = new HttpClient(handler);
+            string result =  await httpClient.GetStringAsync(url);
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                var geoCodeData = JsonConvert.DeserializeObject<GeocodingParser>(result);
+                if (geoCodeData.status.Contains("OK"))
+                {
+                    placeAddress = geoCodeData.results[0].formatted_address;
+                }
+            }
+            return placeAddress;
+        }
+
+
+        public async Task<string> GetDirectionJsonAsync(LatLng location, LatLng destination, string mapkey)
+        {
+           //https://maps.googleapis.com/maps/api/directions/json?origin=47.6580964,23.5241103&destination=47.661993,23.5523382&mode=driving&key=AIzaSyD5tTtNSJQTE6fdHr26R89VUv-HMu_uHUE
+            //origin
+            string str_origin = "origin" + location.Latitude.ToString() + "," + location.Longitude.ToString();
+
+            //destination
+            string str_destination = "destination" + destination.Latitude.ToString() + "," + destination.Longitude.ToString();
+
+            //Mode
+            string mode = "mode=driving";
+
+            string parameters = str_origin + "&" + str_destination + "&" + mode + "&key=" + mapkey;
+
+            string output = "json";
+
+            string url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+            var handler = new HttpClientHandler();
+            HttpClient client = new HttpClient(handler);
+            string jsonString = await client.GetStringAsync(url);
+
+            return jsonString;
+        }
+        
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,146 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Gms.Maps;
+using Android;
+using System;
+using Android.Gms.Maps.Model;
+using Android.Gms.Location;
+using Android.Support.V4.App;
+using Slot.Helpers;
+
+
+namespace Slot
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        readonly string[] permissionGroup = { Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessCoarseLocation };
+
+        TextView placeTextView;
+        Button getDirectionButton;
+
+        GoogleMap map;
+        FusedLocationProviderClient locationProviderClient;
+        Android.Locations.Location myLastLocation;
+        LatLng myposition;
+        LatLng destinationPoint;
+
+        MapHelpers mapHelper = new MapHelpers();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            RequestPermissions(permissionGroup, 0);
+
+            SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+
+            placeTextView = (TextView)FindViewById(Resource.Id.placeTextView);
+            getDirectionButton = (Button)FindViewById(Resource.Id.getDirectionsButton);
+            getDirectionButton.Click += GetDirectionButton_Click;
+
+
+        }
+
+        private async void GetDirectionButton_Click(object sender, EventArgs e)
+        {
+            string key = Resources.GetString(Resource.String.mapkey);
+            string directionJson = await mapHelper.GetDirectionJsonAsync(myposition, destinationPoint, key);
+            Console.WriteLine(directionJson);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (grantResults.Length < 1)
+            {
+                return;
+            }
+
+            if (grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+            {
+                DisplayLocation();
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            var mapStyle = MapStyleOptions.LoadRawResourceStyle(this, Resource.Raw.mapstyle);
+            googleMap.SetMapStyle(mapStyle);
+            map = googleMap;
+
+            map.UiSettings.ZoomControlsEnabled = true;
+            map.CameraMoveStarted += Map_CameraMoveStarted;
+            map.CameraChange += Map_CameraIdle;
+
+
+            if (CheckPermission())
+            {
+                DisplayLocation();
+            }
+            DisplayLocation();
+        }
+
+        private async void Map_CameraIdle(object sender, GoogleMap.CameraChangeEventArgs e)
+        {
+
+            destinationPoint = map.CameraPosition.Target;
+            string key = Resources.GetString(Resource.String.mapkey);
+            string address = await mapHelper.FindCoordinateAddress(destinationPoint, key);
+
+            if (!string.IsNullOrEmpty(address))
+            {
+                placeTextView.Text = address;
+            }
+            else
+            {
+
+                placeTextView.Text = "Incotro?";
+            }
+        }
+
+        private void Map_CameraMoveStarted(object sender, GoogleMap.CameraMoveStartedEventArgs e)
+        {
+
+            placeTextView.Text = "Se seteaza o noua locatie";
+        }
+
+        bool CheckPermission()
+        {
+            bool permissionGranted = false;
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Android.Content.PM.Permission.Granted &&
+                ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Android.Content.PM.Permission.Granted)
+            {
+                permissionGranted = false;
+            }
+            else
+            {
+                permissionGranted = true;
+            }
+            return permissionGranted;
+        }
+        async void DisplayLocation()
+        {
+            if (locationProviderClient == null)
+            {
+                locationProviderClient = LocationServices.GetFusedLocationProviderClient(this);
+            }
+            myLastLocation = await locationProviderClient.GetLastLocationAsync();
+            if (myLastLocation != null)
+            {
+                myposition = new LatLng(myLastLocation.Latitude, myLastLocation.Longitude);
+                map.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(myposition, 15));
+            }
+        }
+    }
+
+}
